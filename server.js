55cd769aca669a8ed50f529655239e4fb43b7461
p-alvarenga <p.alvarenga.afonso@gmail.com>
2025-02-19 11:27:29 -0300

    front end update

--- server.js
+++ server.js
@@ -3,14 +3,15 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = 3000;
+const port = 8000;
 
 //const routes = require(
+app.use(express.static('public'));
 
 const publicPath = path.join(__dirname, 'public')
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'));
+	res.sendFile(path.join(path.join(__dirname, 'public'), 'index.html'));
 });
 
 app.listen(port, () => {
